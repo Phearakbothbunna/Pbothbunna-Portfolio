48f64311f803a85692e2a0cbe787dfc49b877081
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2025-01-27 20:24:04 -0800

    Added more info and styling

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// form submission (client-side)
-// wait for DOM to fully load before running the script
-document.addEventListener('DOMContentLoaded', () => {
-    const cmtInput = document.getElementById("cmtInput");
-    const cmtList = document.getElementById("cmtList");
-    const submitBtn = document.getElementById("submitBtn");
-    // get the user click
-    submitBtn.addEventListener('click', () => {
-        // get input value
-        const cmtText = cmtInput.value;
-        if (cmtText) {
-            // create new list item
-            const newCmt = document.createElement('li');
-            newCmt.textContent = cmtText;
-            cmtList.append(newCmt);
-            cmtInput.value = '';
+
+// For closing the menu automatically when a link in the navbar is clicked
+// Select all nav links
+const navLinks = document.querySelectorAll('.nav-link');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+// Add click event listener to every link in the navbar
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // If the menu is currenlty expanded, close it when a link is clicked
+        if (navbarCollapse.classList.contains('show')) {
+            // create new instance of Collapse component and do toggle
+            new bootstrap.Collapse(navbarCollapse).toggle();
         }
-    })
-})
+    });
+});
